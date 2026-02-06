6a126c9f33290deb7298152bc18ac27e29f0bc60
spiehdid <a.smirnov@eywa.fi>
2026-02-06 19:29:08 +0500

    Transfer prisma files to another folder

--- backend/src/common/prisma/prisma.service.ts
+++ backend/src/common/prisma/prisma.service.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../generated/prisma/client';
 
 const adapter = new PrismaBetterSqlite3({
   url: process.env.DATABASE_URL!,
